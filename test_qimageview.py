0373e9ab4be697a3436dabc49d304659f481f355
Hans Meine <meine@informatik.uni-hamburg.de>
2009-10-22 16:55:23 +0200

    add more unit tests and implement simple 8bit-image view

--- test_qimageview.py
+++ test_qimageview.py
@@ -4,8 +4,9 @@ from PyQt4 import QtGui
 from nose.tools import raises
 
 qimg = QtGui.QImage(320, 240, QtGui.QImage.Format_Indexed8)
+qimg.setNumColors(256)
 
-def test_qimageview():
+def test_viewcreation():
 	v = qimageview.qimageview(qimg)
 	assert v.shape == (240, 320)
 
@@ -20,3 +21,24 @@ def test_qimageview_manyargs():
 @raises(TypeError)
 def test_qimageview_wrongarg():
 	v = qimageview.qimageview(42)
+
+def test_data_access():
+	qimg.fill(42)
+	v = qimageview.qimageview(qimg)
+	assert v.shape == (240, 320)
+	assert v[10,10] == 42
+
+def test_being_view():
+	qimg.fill(23)
+	v = qimageview.qimageview(qimg)
+	qimg.fill(42)
+	assert v.shape == (240, 320)
+	assert v[10,10] == 42
+
+def test_coordinate_access():
+	qimg.fill(0)
+	qimg.setPixel(12, 10, 42)
+	v = qimageview.qimageview(qimg)
+	qimg.fill(42)
+	assert v.shape == (240, 320)
+	assert v[10,12] == 42
