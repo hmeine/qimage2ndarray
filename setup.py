3de9b925058364629da612c0250183404f95b5f7
Hans Meine <meine@informatik.uni-hamburg.de>
2009-11-30 18:02:37 +0100

    add lots of metadata to setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,46 @@ class build_ext(sipdistutils.build_ext):
 setup(name = 'qimage2ndarray',
 	  version = '0.1',
 	  description = 'Conversion between QImages and numpy.ndarrays.',
+	  author = "Hans Meine",
+	  author_email = "meine@informatik.uni-hamburg.de",
+	  url = "http://kogs-www.informatik.uni-hamburg.de/~meine/software/qimage2ndarray",
+#	  download_url = "....tgz",
+	  keywords = ["QImage", "numpy", "ndarray", "image", "convert", "PyQt4"],
 	  packages = ['qimage2ndarray'],
 	  ext_modules = [qimageview],
-	  cmdclass = {'build_ext': build_ext})
+	  cmdclass = {'build_ext': build_ext},
+	  long_description = """\
+qimage2ndarray is a small python extension for quickly converting
+between QImages and numpy.ndarrays (in both directions).  These are
+very common tasks when programming e.g. scientific visualizations in
+Python using PyQt4 as the GUI library.
+
+* Supports conversion of scalar and RGB data, with arbitrary dtypes
+  and memory layout, with and without alpha channels, into QImages
+  (e.g. for display or saving using Qt).
+
+* Using a tiny C++ extension, qimage2ndarray makes it possible to
+  create ndarrays that are *views* into a given QImage's memory.
+
+  This allows for very efficient data handling and makes it possible
+  to modify Qt image data in-place (e.g. for brightness/gamma or alpha
+  mask modifications).
+
+* `Masked arrays`_ are also supported and are converted into QImages
+  with transparent pixels.
+
+* Supports value scaling / normalization to 0..255 for convenient
+  display of arbitrary NumPy arrays.
+""",
+	  classifiers = [
+	"Programming Language :: Python",
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: Developers",
+	"Intended Audience :: Science/Research",
+	"License :: OSI Approved :: BSD License",
+	"Operating System :: OS Independent",
+	"Topic :: Multimedia :: Graphics",
+	"Topic :: Software Development :: Libraries :: Python Modules",
+	"Topic :: Software Development :: User Interfaces",
+	]
+)
