765bd094180395644d2b076be53b8503b49bed4b
Hans Meine <hans_meine@gmx.net>
2013-11-02 06:46:24 +0100

    add more imread() unit tests (against scipy/MPL)

--- test/test_imread.py
+++ test/test_imread.py
@@ -2,10 +2,17 @@ import qimage2ndarray, os, numpy
 from PyQt4 import QtGui
 
 from nose.tools import raises, assert_equal
+from nose.plugins.skip import SkipTest
+
+
+def _locate_test_image(basename):
+    result = os.path.join(os.path.dirname(__file__), basename)
+    assert os.path.exists(result)
+    return result
+
 
 def test_imread_box():
-    filename = os.path.join(os.path.dirname(__file__), 'test_box.png')
-    assert os.path.exists(filename)
+    filename = _locate_test_image('test_box.png')
 
     gray = qimage2ndarray.imread(filename)
 
@@ -18,8 +25,7 @@ def test_imread_box():
 
     
 def test_imread_colored():
-    filename = os.path.join(os.path.dirname(__file__), 'test_colored.jpg')
-    assert os.path.exists(filename)
+    filename = _locate_test_image('test_colored.jpg')
 
     rgb = qimage2ndarray.imread(filename)
 
@@ -32,8 +38,7 @@ def test_imread_colored():
 
     
 def test_imread_colored():
-    filename = os.path.join(os.path.dirname(__file__), 'test_gray.jpg')
-    assert os.path.exists(filename)
+    filename = _locate_test_image('test_gray.jpg')
 
     gray = qimage2ndarray.imread(filename)
 
@@ -42,3 +47,46 @@ def test_imread_colored():
     assert_equal(numpy.ndim(gray), 2)
 
     assert_equal(gray.shape, (11, 16))
+
+
+all_test_images = map(_locate_test_image, ['test_box.png', 'test_colored.jpg', 'test_gray.jpg'])
+
+def test_imread_against_scipy_ndimage():
+    try:
+        import scipy.ndimage
+    except ImportError:
+        raise SkipTest
+
+    for filename in all_test_images:
+        a = scipy.ndimage.imread(filename)
+        b = qimage2ndarray.imread(filename)
+        assert_equal(a.shape, b.shape)
+        assert numpy.all(a == b)
+
+def test_imread_against_scipy_misc():
+    try:
+        import scipy.misc
+    except ImportError:
+        raise SkipTest
+
+    for filename in all_test_images:
+        a = scipy.misc.imread(filename)
+        b = qimage2ndarray.imread(filename)
+        print a.shape, b.shape
+        assert_equal(a.shape, b.shape)
+        assert numpy.all(a == b)
+
+def test_imread_against_matplotlib():
+    try:
+        import matplotlib.pyplot
+    except ImportError:
+        raise SkipTest
+
+    for filename in all_test_images:
+        a = matplotlib.pyplot.imread(filename)
+        b = qimage2ndarray.imread(filename)
+        print a.shape, b.shape
+        assert_equal(a.shape, b.shape)
+        if a.max() == 1.0: # strange MPL API: only the PNG has 0..1.0 range
+            a *= 255
+        assert numpy.all(numpy.abs(a - b) < 1e-8)
