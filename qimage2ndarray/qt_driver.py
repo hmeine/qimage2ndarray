05d860669640a251a85ccb59f4aa25b23ab4add2
Hans Meine <hans_meine@gmx.net>
2015-06-12 07:37:39 +0200

    also support QT_API=pyqt5 (as in ETS/MPL)

--- qimage2ndarray/qt_driver.py
+++ qimage2ndarray/qt_driver.py
@@ -83,7 +83,7 @@ class QtDriver(object):
             drv = os.environ.get('QT_API')
         if drv is None:
             drv = self.DEFAULT
-        drv = {'pyside' : 'PySide', 'pyqt' : 'PyQt4'}.get(drv, drv) # map ETS syntax
+        drv = {'pyside' : 'PySide', 'pyqt' : 'PyQt4', 'pyqt5' : 'PyQt5'}.get(drv, drv) # map ETS syntax
         assert drv in self.DRIVERS
         self._drv = drv
 
