9e2921f394ebc417976ddc6c9ecaec5cbcb7fd29
Hans Meine <hans_meine@gmx.net>
2021-04-16 17:42:21 +0200

    adapt tests to flake8 requirements

--- test/test_qimage_views.py
+++ test/test_qimage_views.py
@@ -14,9 +14,10 @@ def test_raw_indexed8():
     qimg.fill(23)
     qimg.setPixel(12, 10, 42)
     assert_equal(v.shape, (240, 320))
-    assert_equal(v[10,10], 23)
-    assert_equal(v[10,12], 42)
+    assert_equal(v[10, 10], 23)
+    assert_equal(v[10, 12], 42)
     assert_equal(v.nbytes, numBytes(qimg))
+
 
 # Format_RGB32 = 4
 def test_raw_rgb32():
@@ -24,11 +25,12 @@ def test_raw_rgb32():
     qimg.fill(0)
     v = qimage2ndarray.raw_view(qimg)
     qimg.fill(23)
-    qimg.setPixel(12, 10, QtGui.qRgb(0,0,42))
+    qimg.setPixel(12, 10, QtGui.qRgb(0, 0, 42))
     assert_equal(v.shape, (240, 320))
-    assert_equal(v[10,10], 23 | 0xff000000)
-    assert_equal(v[10,12], 42 | 0xff000000)
+    assert_equal(v[10, 10], 23 | 0xff000000)
+    assert_equal(v[10, 12], 42 | 0xff000000)
     assert_equal(v.nbytes, numBytes(qimg))
+
 
 # Format_RGB16 = 4
 def test_raw_rgb16():
@@ -36,11 +38,12 @@ def test_raw_rgb16():
     qimg.fill(0)
     v = qimage2ndarray.raw_view(qimg)
     qimg.fill(23)
-    qimg.setPixel(12, 10, QtGui.qRgb(0,0,91))
+    qimg.setPixel(12, 10, QtGui.qRgb(0, 0, 91))
     assert_equal(v.shape, (240, 320))
-    assert_equal(v[10,10], 23)
-    assert_equal(v[10,12], 91>>3)
+    assert_equal(v[10, 10], 23)
+    assert_equal(v[10, 12], 91 >> 3)
     assert_equal(v.nbytes, numBytes(qimg))
+
 
 # Format_Grayscale8 = 24 (Qt 5.5+)
 def test_raw_grayscale8():
@@ -50,9 +53,10 @@ def test_raw_grayscale8():
     qimg.fill(1)
     qimg.setPixel(12, 10, QtGui.qRgb(42, 42, 42))
     assert_equal(v.shape, (240, 320))
-    assert_equal(v[10,10], 1)
-    assert_equal(v[10,12], 42)
+    assert_equal(v[10, 10], 1)
+    assert_equal(v[10, 12], 42)
     assert_equal(v.nbytes, numBytes(qimg))
+
 
 # Format_RGBA64 = 26 (Qt 5.12+)
 def test_raw_rgba64():
@@ -60,13 +64,15 @@ def test_raw_rgba64():
     qimg.fill(0)
     v = qimage2ndarray.raw_view(qimg)
     qimg.fill(1)
-    qimg.setPixel(12, 10, QtGui.qRgb(0x12,0x34,0x56))
+    qimg.setPixel(12, 10, QtGui.qRgb(0x12, 0x34, 0x56))
     assert_equal(v.shape, (240, 320))
-    assert_equal(v[10,10], 0x010100000000)
-    assert_equal(v[10,12], 0xffff565634341212)
+    assert_equal(v[10, 10], 0x010100000000)
+    assert_equal(v[10, 12], 0xffff565634341212)
     assert_equal(v.nbytes, numBytes(qimg))
 
-#---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
 
 def test_byte_view_rgb32():
     qimg = QtGui.QImage(320, 240, QtGui.QImage.Format_RGB32)
@@ -74,9 +80,10 @@ def test_byte_view_rgb32():
     qimg.fill(23)
     qimg.setPixel(12, 10, 42)
     assert_equal(v.shape, (240, 320, 4))
-    assert_equal(list(v[10,10]), [23, 0, 0, 0xff])
-    assert_equal(list(v[10,12]), [42, 0, 0, 0xff])
+    assert_equal(list(v[10, 10]), [23, 0, 0, 0xff])
+    assert_equal(list(v[10, 12]), [42, 0, 0, 0xff])
     assert_equal(v.nbytes, numBytes(qimg))
+
 
 def test_byte_view_indexed():
     qimg = QtGui.QImage(320, 240, QtGui.QImage.Format_Indexed8)
@@ -85,35 +92,38 @@ def test_byte_view_indexed():
     qimg.fill(23)
     qimg.setPixel(12, 10, 42)
     assert_equal(v.shape, (240, 320, 1))
-    assert_equal(list(v[10,10]), [23])
-    assert_equal(list(v[10,12]), [42])
+    assert_equal(list(v[10, 10]), [23])
+    assert_equal(list(v[10, 12]), [42])
     assert_equal(v.nbytes, numBytes(qimg))
+
 
 def test_rgb_view():
     qimg = QtGui.QImage(320, 240, QtGui.QImage.Format_RGB32)
-    qimg.fill(QtGui.qRgb(23,0,0))
+    qimg.fill(QtGui.qRgb(23, 0, 0))
     v = qimage2ndarray.rgb_view(qimg)
-    qimg.setPixel(12, 10, QtGui.qRgb(12,34,56))
-    assert_equal(list(v[10,10]), [23,0,0])
-    assert_equal(list(v[10,12]), [12,34,56])
+    qimg.setPixel(12, 10, QtGui.qRgb(12, 34, 56))
+    assert_equal(list(v[10, 10]), [23, 0, 0])
+    assert_equal(list(v[10, 12]), [12, 34, 56])
+
 
 def test_alpha_view():
     qimg = QtGui.QImage(320, 240, QtGui.QImage.Format_ARGB32)
     qimg.fill(0)
     v = qimage2ndarray.alpha_view(qimg)
-    qimg.setPixel(12, 10, QtGui.qRgb(12,34,56))
-    assert_equal(v[10,12], 255)
-    assert_equal(v[10,11], 0)
+    qimg.setPixel(12, 10, QtGui.qRgb(12, 34, 56))
+    assert_equal(v[10, 12], 255)
+    assert_equal(v[10, 11], 0)
+
 
 def test_recarray_view():
     qimg = QtGui.QImage(320, 240, QtGui.QImage.Format_ARGB32)
     qimg.fill(23)
     v = qimage2ndarray.recarray_view(qimg)
-    qimg.setPixel(12, 10, QtGui.qRgb(12,34,56))
-    assert_equal(v["g"][10,12], 34)
+    qimg.setPixel(12, 10, QtGui.qRgb(12, 34, 56))
+    assert_equal(v["g"][10, 12], 34)
     assert_equal(v["g"].sum(), 34)
     assert_equal(v["green"].sum(), 34)
-    assert_equal(v.g[10,12], 34)
+    assert_equal(v.g[10, 12], 34)
     # this worked in the past, but with NumPy 1.2.1, I get:
     # TypeError: function takes at most 2 arguments (3 given)
-    assert_equal(v[10,12]["g"], 34)
+    assert_equal(v[10, 12]["g"], 34)
